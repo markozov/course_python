b6d32ac443f404e4f0ed45e5127879976291cbe7
markozov <147277400+markozov@users.noreply.github.com>
2023-11-15 20:31:44 +0300

    Update lab4_2.py

--- Лабораторная работа 4/lab4_2.py
+++ Лабораторная работа 4/lab4_2.py
@@ -1,18 +1,27 @@
 # TODO импортировать необходимые молули
-
+import csv
+import json
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    ...  # TODO считать содержимое csv файла
+      # TODO считать содержимое csv файла
+      data = []
+      with open(INPUT_FILENAME, 'r') as file:
+          reader = csv.DictReader(file)
+          for row in reader:
+              data.append(row)
 
-    ...  # TODO Сериализовать в файл с отступами равными 4
+      with open(OUTPUT_FILENAME, 'w') as file:
+          json.dump(data, file, indent=4)
+
+      # TODO Сериализовать в файл с отступами равными 4
 
 
 if __name__ == '__main__':
-    # Нужно для проверки
+     #Нужно для проверки
     task()
 
     with open(OUTPUT_FILENAME) as output_f:
